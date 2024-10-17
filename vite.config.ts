09e6c806887574ec23c536a45b232fb82ada6e5f
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2024-10-17 22:28:38 +0200

    test: Configure coverage

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      include: [
+        'src/**/*.{mjs,mjsx,js,jsx,ts,tsx}',
+        '!src/stories/**',
+        '!src/**/*.stories.ts',
+      ],
+      provider: 'istanbul',
+    },
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/__tests__/setup.ts'],
