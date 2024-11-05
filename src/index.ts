b4508fe58512ffdf304c91a37af9e217b022e920
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2024-11-05 09:42:17 +0100

    fix: Add missing TypeScript exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 export * from './utils/tag'
 export { defaultConfig } from './defaultConfig'
+
+export type { Badge, BadgeOrBadgeFn } from './types/Badge'
+export type { Display } from './types/DisplayOption'
+export type { TagBadgeParameters } from './types/TagBadgeParameters'
+export type { TagPattern, TagPatterns } from './types/TagPattern'
