73b8acc44726101353cb8f5463023e74c07110c9
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-01-16 09:33:06 +0100

    refactor: Move badge stories to examples

--- src/examples/Badge.stories.ts
+++ src/examples/Badge.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Badge } from './Badge'
+import { Badge } from '../components/Badge'
 
 const meta: Meta<typeof Badge> = {
   title: 'Addon/Badge',
