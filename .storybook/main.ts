0d2b131a04815e569f5e0179853bc3287bff5457
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-03-20 22:34:21 +0100

    chore: Install Test Addon internally

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,11 @@ import type { StorybookConfig } from '@storybook/react-vite'
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 
-  addons: ['./local-preset.js', '@storybook/addon-essentials'],
+  addons: [
+    './local-preset.js',
+    '@storybook/addon-essentials',
+    '@storybook/experimental-addon-test',
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin'
 import react from '@vitejs/plugin-react'
 
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const dirname =
+  typeof __dirname !== 'undefined'
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    storybookTest({
+      configDir: path.join(dirname, '.storybook'),
+      storybookScript: 'pnpm start --ci',
+    }),
+  ],
   test: {
     coverage: {
       include: [
@@ -16,6 +32,12 @@ export default defineConfig({
     },
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/__tests__/setup.ts'],
+    setupFiles: ['./src/__tests__/setup.ts', './.storybook/vitest.setup.ts'],
+    browser: {
+      enabled: true,
+      name: 'chromium',
+      provider: 'playwright',
+      headless: true,
+    },
   },
 })
