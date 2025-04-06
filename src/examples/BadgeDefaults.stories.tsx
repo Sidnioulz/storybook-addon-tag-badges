202abf6276a76885c13e1059c0d09b21b4790440
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-04-06 11:18:23 +0200

    feat: Port to Storybook 9.0.0-alpha.17
    BREAKING CHANGE: This addon now requires Storybook 9 to run.

--- src/examples/BadgeDefaults.stories.tsx
+++ src/examples/BadgeDefaults.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 
 import { Badge, getBadgeProps } from '../components/Badge'
 import { defaultConfig } from '../defaultConfig'
