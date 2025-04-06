69c7c216526b13c80b11dd9bfe86e32967adbf86
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-04-06 11:18:23 +0200

    feat: Port to Storybook 9.0.0-alpha.17
    BREAKING CHANGE: This addon now requires Storybook 9 to run.
    
    Signed-off-by: Steve Dodier-Lazaro <sidnioulz@gmail.com>

--- src/examples/BadgeWorkflows.stories.tsx
+++ src/examples/BadgeWorkflows.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 import { Title, Stories } from '@storybook/blocks'
 
 import { Badge, getBadgeProps } from '../components/Badge'
--- src/stories/Frog.stories.ts
+++ src/stories/Frog.stories.ts
@@ -1,4 +1,5 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
 import { Frog } from './Frog'
 
 const meta: Meta<typeof Frog> = {
