91f961c07a99d4c5bd04a75c341e45ce8cacf1a6
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-04-26 15:09:35 +0200

    fix: Handle margin alignment with the test addon
    Signed-off-by: Steve Dodier-Lazaro <sidnioulz@gmail.com>

--- src/renderLabel.tsx
+++ src/renderLabel.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
-import { API_HashEntry } from '@storybook/types'
+import { API_HashEntry, type StatusByTypeId } from '@storybook/types'
+import { experimental_useStatusStore } from 'storybook/manager-api'
 
 import { Sidebar } from './components/Sidebar'
+
+function hasStatusWithUI(itemStatuses: StatusByTypeId): boolean {
+  if (!itemStatuses) {
+    return false
+  }
+
+  if (itemStatuses['storybook/component-test']) {
+    return true
+  }
+
+  // Add future statuses with a UI element here.
+
+  return false
+}
 
 export function renderLabel(item: API_HashEntry) {
   if (
@@ -13,5 +28,11 @@ export function renderLabel(item: API_HashEntry) {
     return
   }
 
-  return <Sidebar item={item}>{item.name}</Sidebar>
+  const itemStatuses = experimental_useStatusStore((all) => all[item.id])
+
+  return (
+    <Sidebar item={item} hasStatusWithUI={hasStatusWithUI(itemStatuses)}>
+      {item.name}
+    </Sidebar>
+  )
 }
