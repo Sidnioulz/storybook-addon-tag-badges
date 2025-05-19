7868d550db24d149674d8b0de27ddce6aa81101e
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-05-19 19:34:51 +0200

    test: Restructure tests to reintroduce unit tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
 
 export default defineConfig({
-  plugins: [react(), storybookTest({ storybookScript: 'pnpm storybook --ci' })],
+  plugins: [react()],
   test: {
     coverage: {
       include: [
@@ -17,16 +16,6 @@ export default defineConfig({
     },
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/__tests__/setup.ts', './.storybook/vitest.setup.ts'],
-    browser: {
-      enabled: true,
-      provider: 'playwright',
-      headless: true,
-      instances: [
-        {
-          browser: 'chromium',
-        },
-      ],
-    },
+    setupFiles: ['./src/__tests__/setup.ts'],
   },
 })
