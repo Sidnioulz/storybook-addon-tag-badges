ea302fb35c76cc94e271404c8a338c2248c74bb0
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-09-10 22:13:59 +0200

    fix: Pass config to preview without requiring addon imports in user preview context
    Signed-off-by: Steve Dodier-Lazaro <sidnioulz@gmail.com>

--- src/components/CustomBadge.tsx
+++ src/components/CustomBadge.tsx
@@ -1,8 +1,8 @@
-import React, { type FC } from 'react'
+import React from 'react'
 
 import { Badge } from './Badge'
 import type { Badge as BadgeConfigType } from '../types/Badge'
 
-export const CustomBadge: FC<BadgeConfigType> = (props) => {
+export const CustomBadge: React.FC<BadgeConfigType> = (props) => {
   return <Badge context="mdx" {...props} />
 }
