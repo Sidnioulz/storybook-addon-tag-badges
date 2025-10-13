fa8d9bea0bfc74398900c326decbce1e8d9417b8
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-10-13 11:27:56 +0200

    feat: Port to Storybook 10
    BREAKING CHANGE: This addon now requires Storybook 10.0 or newer to work.
    
    Signed-off-by: Steve Dodier-Lazaro <sidnioulz@gmail.com>

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,35 +1,13 @@
-import { readFile } from 'node:fs/promises'
-
 import { defineConfig, type Options } from 'tsup'
-import { globalPackages as globalManagerPackages } from 'storybook/internal/manager/globals'
-import { globalPackages as globalPreviewPackages } from 'storybook/internal/preview/globals'
 
-const BROWSER_TARGETS: Options['target'] = [
-  'chrome131',
-  'edge134',
-  'firefox136',
-  'safari18.3',
-  'ios18.3',
-  'opera117',
-]
-const NODE_TARGET: Options['target'] = ['node20']
+const NODE_TARGET = 'node20.19'
 
-type BundlerConfig = {
-  bundler?: {
-    exportEntries?: string[]
-    nodeEntries?: string[]
-    managerEntries?: string[]
-    previewEntries?: string[]
-  }
-}
-
-export default defineConfig(async (options) => {
-  const packageJson = (await readFile('./package.json', 'utf8').then(
-    JSON.parse,
-  )) as BundlerConfig
+export default defineConfig(async () => {
+  const packageJson = (
+    await import('./package.json', { with: { type: 'json' } })
+  ).default
   const {
     bundler: {
-      exportEntries = [],
       managerEntries = [],
       previewEntries = [],
       nodeEntries = [],
@@ -37,31 +15,14 @@ export default defineConfig(async (options) => {
   } = packageJson
 
   const commonConfig: Options = {
-    splitting: false,
-    minify: !options.watch,
+    splitting: true,
+    format: ['esm'],
     treeshake: true,
-    sourcemap: true,
     clean: false,
+    external: ['react', 'react-dom', '@storybook/icons'],
   }
 
   const configs: Options[] = []
-
-  // export entries are entries meant to be manually imported by the user
-  // they are not meant to be loaded by the manager or preview
-  // they'll be usable in both node and browser environments, depending on which features and modules they depend on
-  if (exportEntries.length) {
-    configs.push({
-      ...commonConfig,
-      entry: exportEntries,
-      dts: {
-        resolve: true,
-      },
-      format: ['esm', 'cjs'],
-      platform: 'neutral',
-      target: NODE_TARGET,
-      external: [...globalManagerPackages, ...globalPreviewPackages],
-    })
-  }
 
   // manager entries are entries meant to be loaded into the manager UI
   // they'll have manager-specific packages externalized and they won't be usable in node
@@ -70,10 +31,8 @@ export default defineConfig(async (options) => {
     configs.push({
       ...commonConfig,
       entry: managerEntries,
-      format: ['esm'],
       platform: 'browser',
-      target: BROWSER_TARGETS,
-      external: globalManagerPackages,
+      target: 'esnext',
     })
   }
 
@@ -84,14 +43,9 @@ export default defineConfig(async (options) => {
     configs.push({
       ...commonConfig,
       entry: previewEntries,
-      dts: {
-        resolve: true,
-      },
-      format: ['esm', 'cjs'],
       platform: 'browser',
-      target: BROWSER_TARGETS,
-      external: globalPreviewPackages,
-      // FIXME/TODO: react, react-dom, and react/jsx-runtime?
+      target: 'esnext',
+      dts: true,
     })
   }
 
@@ -102,7 +56,6 @@ export default defineConfig(async (options) => {
     configs.push({
       ...commonConfig,
       entry: nodeEntries,
-      format: ['cjs'],
       platform: 'node',
       target: NODE_TARGET,
     })
