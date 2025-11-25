09db268cab1800f2c1a25cb8a00671dc94e633b1
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-11-25 16:15:05 +0100

    chore: Add Chromatic

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,7 @@ const config: StorybookConfig = {
     import.meta.resolve('./local-preset.ts'),
     '@storybook/addon-docs',
     '@storybook/addon-vitest',
+    '@chromatic-com/storybook',
   ],
   framework: {
     name: '@storybook/react-vite',
